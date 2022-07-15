affa3b6b5c3aeb6be352fb518ff3365694efdcca
mikeyo98 <mikeyo98@users.noreply.github.com>
2022-07-14 19:49:38 -0700

    Global Configuration added
    - allow all cros requests
    - note: try the recommended config after all basic back end components are done

---
+++ src/main/java/com/mz/tmall/config/WebConfig.java
@@ -0,0 +1,44 @@
+package com.mz.tmall.config;
+/*
+ cors config tutorial:
+    https://spring.io/blog/2015/06/08/cors-support-in-spring-framework
+*/
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+@EnableWebMvc // not included in the project
+public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
+/*
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+                    .allowedHeaders("*");
+            }
+        };
+    }
+}
+
+
+
+
+ */
